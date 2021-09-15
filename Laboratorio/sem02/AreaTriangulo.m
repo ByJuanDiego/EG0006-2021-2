ad7b083e0e9b258cdbc1943b0e37daa370ab74bc
ByJuanDiego <juancaspadi@gmail.com>
2021-09-15 12:38:59 -0500

    a

---
+++ Laboratorio/sem02/AreaTriangulo.m
@@ -0,0 +1,15 @@
+function [Area] = AreaTriangulo(x1, y1, x2, y2, x3, y3);
+ %{
+    Esta función calcula el área de un triángulo.
+    Recibe como parámetros 3 puntos.
+ %}
+
+ M = [
+       x1 y1 1;
+       x2 y2 1;
+       x3 y3 1
+];
+
+Area = (1/2)*det(M);
+end
+
