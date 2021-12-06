821dddc53ae2d6d74d9063802748efc7d5bc593e
ByJuanDiego <juancaspadi@gmail.com>
2021-12-05 21:30:12 -0500

    Update FactorR2.m

--- Metodos Numericos/Aproximacion De Funciones/FactorR2.m
+++ Metodos Numericos/Aproximacion De Funciones/FactorR2.m
@@ -3,10 +3,12 @@ function [R2] = FactorR2(x,y)
     % Esta función calcula el coeficiente de determinación R^2 del ajuste
     % lineal de datos
     
+    P = polyfit(x, y, 1);
+    yi = polyval(P, x);
+    
     n = length(x);
-    g1 = polyfit(x,y,1);
-    yis = polyval(g1,x);
     ym = sum(y)/n;
-    R2 = sum((yis-ym).^2)/sum((y-ym).^2);
+    
+    R2 = sum((yi - ym).^2)/sum((y - ym).^2);
     
 end
