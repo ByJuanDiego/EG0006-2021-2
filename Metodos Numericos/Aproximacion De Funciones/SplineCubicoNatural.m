5d2a6067b975f243ca0c92500017eead23de9b61
ByJuanDiego <juancaspadi@gmail.com>
2021-12-06 21:52:45 -0500

    a

--- Metodos Numericos/Aproximacion De Funciones/SplineCubicoNatural.m
+++ Metodos Numericos/Aproximacion De Funciones/SplineCubicoNatural.m
@@ -1,19 +1,16 @@
-function [S, T]=SplineCubicoNatural(x,y)
+function [S, T]=SplineCubicoNatural(x, y)
     
     N = length(x)-1;
-    
     H = diff(x);
     E = diff(y)./H;
-    
     diagprinc = 2*(H(1:N-1)+H(2:N));
     diagsupinf = H(2:N-1);
-    
 	g0 = 0;
     gn = 0;
-    
 	A = diag(diagprinc)+diag(diagsupinf,1)+diag(diagsupinf,-1);
-	b = 6*diff(E'); g=A\b;
-	g = [g0 g' gn];
+	b = 6*diff(E'); 
+    g=A\b; 
+    g = [g0 g' gn];
     
     variableNames = {'x','y','Δx', 'Δy/Δx', 'S"(x)'};
     T = table(x',y',[H nan]',[E nan]',g', VariableNames=variableNames);
@@ -32,25 +29,47 @@ function [S, T]=SplineCubicoNatural(x,y)
         yy = S(i,1)*(xx - x(i)).^3 + S(i,2)*(xx - x(i)).^2 + S(i,3)*(xx - x(i)) + S(i,4);
         plot(xx, yy), hold on
         
-        s = "S(" + num2str(i-1) + ") = ";
-        r = "*(x-" + num2str(x(i)) + ")";
-        a3 = num2str(S(i, 1));
-        a2 = num2str(S(i, 2));
-        a1 = num2str(S(i, 3));
-        a0 = num2str(S(i, 4));
-        xinf = num2str(x(i));
-        xsup = num2str(x(i+1));
-        fprintf(s ...
-            + "+ " + a3 + r + "^3   "...
-            + "+ " + a2 + r + "^2   "...
-            + "+ " + a1 + r + "   "...
-            + "+ " + a0...
-            + "   si x ∈ [" + xinf + ", " + xsup + "]\n" ...
-            )
+        ImprimirS(S, x, i);
         
     end
     
 	grid on
     hold off
+    
+end
+
+
+function [] = ImprimirS(S, x, i)
+
+    s = "S(" + num2str(i-1) + ") = ";
+    r = "*(x-" + num2str(x(i)) + ")";
+    a3 = num2str(abs(S(i, 1)));
+    a2 = num2str(abs(S(i, 2)));
+    a1 = num2str(abs(S(i, 3)));
+    a0 = num2str(abs(S(i, 4)));
+    xinf = num2str(x(i));
+    xsup = num2str(x(i+1));
+    sign3 = "  ";
+    if S(i, 1) < 0
+        sign3 = "- ";
+    end
+    sign2 = "+ ";
+    if S(i, 2) < 0
+        sign2 = "- ";
+    end
+    sign1 = "+ ";
+    if S(i, 3) < 0
+        sign1 = "- ";
+    end
+    sign0 = "+ ";
+    if S(i, 4) < 0
+        sign0 = "- ";
+    end
+    fprintf(s ...
+        + sign3 + a3 + r + "^3   "...
+        + sign2 + a2 + r + "^2   "...
+        + sign1 + a1 + r + "  "...
+        + sign0 + a0...
+        + "   si x ∈ [" + xinf + ", " + xsup + "]\n")
     
 end
