0c0dec086235d4f80d2fc34e5e61c0718f9ae9ba
ByJuanDiego <juancaspadi@gmail.com>
2021-12-06 22:08:06 -0500

    a

--- Algebra Lineal/Valores y Vectores Propios/PotenciaMAXITER.m
+++ Algebra Lineal/Valores y Vectores Propios/PotenciaMAXITER.m
@@ -1,28 +1,23 @@
 function [T] = PotenciaMAXITER(A, x0, maxIter)
-
-
-
-z = [0 nan nan x0'];
-
-for i=1:maxIter
     
-    xi = A*x0;
-    [~, k] = max(abs(xi));
-    lambda = xi(k);
-    v = (1/lambda)*xi;
+    z = [0 nan nan x0'];
     
-    error = norm(v - x0)/norm(v);
-    z = [z; i lambda error v']; %#ok<AGROW> 
-    x0 = v;
+    for i=1:maxIter
+        
+        xi = A*x0;
+        [~, k] = max(abs(xi));
+        lambda = xi(k);
+        v = (1/lambda)*xi;
+        
+        error = norm(v - x0)/norm(v);
+        z = [z; i lambda error v']; %#ok<AGROW> 
+        x0 = v;
+        
+    end
     
-end
-
-T = table(z(:, 1), z(:, 2), z(:, 4:end), z(:, 3));
-T.Properties.VariableNames{'Var1'} = 'i';
-T.Properties.VariableNames{'Var2'} = 'eigenvalue';
-T.Properties.VariableNames{'Var3'} = 'eigenvector';
-T.Properties.VariableNames{'Var4'} = 'error relativo';
-disp(T);
-
+    varNames = {'i', 'eigenvalue', 'eigenvector', 'error'};
+    T = table(z(:,1),z(:,2),z(:,4:end),z(:,3), VariableNames=varNames);
+    disp(T);
+    
 end
 
--- Algebra Lineal/Valores y Vectores Propios/PotenciaTOL.m
+++ Algebra Lineal/Valores y Vectores Propios/PotenciaTOL.m
@@ -1,28 +1,25 @@
 function [T] = PotenciaTOL(A, x0, Tol)
-
-i = 0;
-error = 1;
-z = [i nan nan x0'];
-
-while error > Tol
     
-    xi = A*x0;
-    [~, k] = max(abs(xi));
-    lambda = xi(k);
-    v = (1/lambda)*xi;
-    i = i + 1;
-    error = norm(v - x0)/norm(v);
-    z = [z; i lambda error v']; %#ok<AGROW> 
-    x0 = v;
+    i = 0;
+    error = 1;
+    z = [i nan nan x0'];
     
-end
-
-T = table(z(:, 1), z(:, 2), z(:, 4:end), z(:, 3));
-T.Properties.VariableNames{'Var1'} = 'i';
-T.Properties.VariableNames{'Var2'} = 'eigenvalue';
-T.Properties.VariableNames{'Var3'} = 'eigenvector';
-T.Properties.VariableNames{'Var4'} = 'Error relativo';
-disp(T);
-
+    while error > Tol
+        
+        xi = A*x0;
+        [~, k] = max(abs(xi));
+        lambda = xi(k);
+        v = (1/lambda)*xi;
+        i = i + 1;
+        error = norm(v - x0)/norm(v);
+        z = [z; i lambda error v']; %#ok<AGROW> 
+        x0 = v;
+        
+    end
+    
+    varNames = {'i', 'eigenvalue', 'eigenvector', 'error'};
+    T = table(z(:,1),z(:,2),z(:,4:end),z(:,3), VariableNames=varNames);
+    disp(T);
+    
 end
 
