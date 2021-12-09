2dec9e48f1e94b184f9b0a80b58ce525bca4c9f2
ByJuanDiego <juancaspadi@gmail.com>
2021-12-09 09:16:57 -0500

    Update RungeKutta4.m

--- Metodos Numericos/SEDO/RungeKutta4.m
+++ Metodos Numericos/SEDO/RungeKutta4.m
@@ -9,17 +9,17 @@ function [T] = RungeKutta4(f, g, a, b, x0, y0, h)
     % x(i+1) = x(i) + h/6*(K1x(i) + 2*K2x(i) + 2*K3x(i) + K4x(i));
     % y(i+1) = y(i) + h/6*(K1y(i) + 2*K2y(i) + 2*K3y(i) + K4y(i));
     % 
-    % K1x(i) = f(t(i), x(i), y(i))
-    % K1y(i) = g(t(i), x(i), y(i))
+    % K1x(i) = h*f(t(i), x(i), y(i))
+    % K1y(i) = h*g(t(i), x(i), y(i))
     % 
-    % K2x(i) = f(t(i)+h/2, x(i)+K1x(i)/2, y(i)+K1y(i)/2)
-    % K2y(i) = g(t(i)+h/2, x(i)+K1x(i)/2, y(i)+K1y(i)/2)
+    % K2x(i) = h*f(t(i)+h/2, x(i)+K1x(i)/2, y(i)+K1y(i)/2)
+    % K2y(i) = h*g(t(i)+h/2, x(i)+K1x(i)/2, y(i)+K1y(i)/2)
     %
-    % K3x(i) = f(t(i)+h/2, x(i)+K2x(i)/2, y(i)+K2y(i)/2);
-    % K3y(i) = g(t(i)+h/2, x(i)+K2x(i)/2, y(i)+K2y(i)/2);
+    % K3x(i) = h*f(t(i)+h/2, x(i)+K2x(i)/2, y(i)+K2y(i)/2);
+    % K3y(i) = h*g(t(i)+h/2, x(i)+K2x(i)/2, y(i)+K2y(i)/2);
     %
-    % K4x(i) = f(t(i)+h, x(i)+K3x(i), y(i)+K3y(i));
-    % K4y(i) = g(t(i)+h, x(i)+K3x(i), y(i)+K3y(i));
+    % K4x(i) = h*f(t(i)+h, x(i)+K3x(i), y(i)+K3y(i));
+    % K4y(i) = h*g(t(i)+h, x(i)+K3x(i), y(i)+K3y(i));
     
     n = (b-a)/h;
     x = x0;
@@ -29,22 +29,22 @@ function [T] = RungeKutta4(f, g, a, b, x0, y0, h)
     
     for i = 0:n
         
-        K1x = f(t, x, y);
-        K1y = g(t, x, y);
+        K1x = h*f(t, x, y);
+        K1y = h*g(t, x, y);
         
-        K2x = f(t+h/2, x+K1x/2, y+K1y/2);
-        K2y = g(t+h/2, x+K1x/2, y+K1y/2);
+        K2x = h*f(t+h/2, x+K1x/2, y+K1y/2);
+        K2y = h*g(t+h/2, x+K1x/2, y+K1y/2);
         
-        K3x = f(t+h/2, x+K2x/2, y+K2y/2);
-        K3y = g(t+h/2, x+K2x/2, y+K2y/2);
+        K3x = h*f(t+h/2, x+K2x/2, y+K2y/2);
+        K3y = h*g(t+h/2, x+K2x/2, y+K2y/2);
         
-        K4x = f(t+h, x+K3x, y+K3y);
-        K4y = g(t+h, x+K3x, y+K3y);
+        K4x = h*f(t+h, x+K3x, y+K3y);
+        K4y = h*g(t+h, x+K3x, y+K3y);
         
         z = [z; i t x y K1x K1y K2x K2y K3x K3y K4x K4y]; %#ok<AGROW> 
         
-        x = x + h/6*(K1x + 2*K2x + 2*K3x + K4x);
-        y = y + h/6*(K1y + 2*K2y + 2*K3y + K4y);
+        x = x + 1/6*(K1x + 2*K2x + 2*K3x + K4x);
+        y = y + 1/6*(K1y + 2*K2y + 2*K3y + K4y);
         t = t + h;
         
     end
